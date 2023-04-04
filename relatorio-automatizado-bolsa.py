38a6b3033b8bc6bd2e7ee9f8e787d46e2aff6368
Rodrigo Cardoso <78482228+rodrigohash@users.noreply.github.com>
2023-04-04 00:47:46 -0300

    Create relatorio-automatizado-bolsa.py
    Codigo do relatorio completo e funcional consumindo dados reais automatizados.

---
+++ relatorio-automatizado-bolsa.py
@@ -0,0 +1,144 @@
+# Importando os módulos necessários
+
+import pandas as pd
+import datetime
+import yfinance as yf
+from matplotlib import pyplot as plt
+import mplcyberpunk
+import smtplib
+from email.message import EmailMessage
+
+# Pegar dados no Yahoo Finance
+
+ativos = ["^BVSP", "BRL=X"]
+
+hoje = datetime.datetime.now()
+um_ano_atras = hoje - datetime.timedelta(days = 365)
+
+dados_mercado = yf.download(ativos, um_ano_atras, hoje)
+
+display(dados_mercado)
+
+# Manipulando os dados - seleção e exclusão de dados
+
+dados_fechamento = dados_mercado['Adj Close']
+dados_fechamento.columns = ['dolar', 'ibovespa']
+dados_fechamento = dados_fechamento.dropna()
+
+dados_fechamento
+
+# Manipulando os dados - Criando tabelas com outros timeframes.
+
+dados_fechamento_mensal = dados_fechamento.resample("M").last()
+dados_fechamento_anual = dados_fechamento.resample("Y").last()
+
+dados_fechamento_anual
+
+#Calcular fechamento do dia, retorno no ano e retorno no mês dos ativos.
+
+retorno_no_ano = dados_fechamento_anual.pct_change().dropna()
+retorno_no_mes = dados_fechamento_mensal.pct_change().dropna()
+retorno_no_dia = dados_fechamento.pct_change().dropna()
+
+retorno_no_dia
+
+# Localizar o fechamento do dia anterior, retorno no mês e retorno no ano.
+## Loc -> referenciar elementos a partir do nome
+## Iloc -> selecionar elementos como uma matriz
+
+retorno_dia_dolar = retorno_no_dia.iloc[-1, 0]
+retorno_dia_ibovespa = retorno_no_dia.iloc[-1, 1]
+
+retorno_mes_dolar = retorno_no_mes.iloc[-1, 0]
+retorno_mes_ibovespa = retorno_no_mes.iloc[-1, 1]
+
+retorno_ano_dolar = retorno_no_ano.iloc[-1, 0]
+retorno_ano_ibovespa = retorno_no_ano.iloc[-1, 1]
+
+retorno_ano_dolar
+
+retorno_dia_dolar = round(retorno_dia_dolar * 100, 2)
+retorno_dia_ibovespa = round(retorno_dia_ibovespa * 100, 2)
+
+retorno_mes_dolar = round(retorno_mes_dolar * 100, 2)
+retorno_mes_ibovespa = round(retorno_mes_ibovespa * 100, 2)
+
+retorno_ano_dolar = round(retorno_ano_dolar * 100, 2)
+retorno_ano_ibovespa = round(retorno_ano_ibovespa * 100, 2)
+
+# Fazer os gráficos da performance do último dos ativos
+
+plt.style.use("cyberpunk")
+
+dados_fechamento.plot(y = 'ibovespa', use_index = True, legend = False)
+
+plt.title("Ibovespa")
+
+plt.savefig('ibovespa.png', dpi = 300)
+
+plt.show()
+
+plt.style.use("cyberpunk")
+
+dados_fechamento.plot(y = 'dolar', use_index = True, legend = False)
+
+plt.title("Dolar")
+
+plt.savefig('dolar.png', dpi = 300)
+
+plt.show()
+
+# Enviar relatorio via e-mail
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+senha = os.environ.get("senha_email")
+email = 'emailteste123@gmail.com'
+
+retorno_ano_ibovespa
+
+msg = EmailMessage()
+msg['Subject'] = "Enviando e-mail com o Python"
+msg['From'] = 'seuemail@gmail.com'
+msg['To'] = 'brenno@varos.com.br'
+
+msg.set_content(f'''Prezado diretor, segue o relatório diário:
+
+Bolsa:
+
+No ano o Ibovespa está tendo uma rentabilidade de {retorno_ano_ibovespa}%, 
+enquanto no mês a rentabilidade é de {retorno_mes_ibovespa}%.
+
+No último dia útil, o fechamento do Ibovespa foi de {retorno_dia_ibovespa}%.
+
+Dólar:
+
+No ano o Dólar está tendo uma rentabilidade de {retorno_ano_dolar}%, 
+enquanto no mês a rentabilidade é de {retorno_mes_dolar}%.
+
+No último dia útil, o fechamento do Dólar foi de {retorno_dia_dolar}%.
+
+
+Abs,
+
+Rodrigo Cardoso
+
+''')
+
+with open('dolar.png', 'rb') as content_file:
+    content = content_file.read()
+    msg.add_attachment(content, maintype='application', subtype='png', filename='dolar.png')
+    
+    
+with open('ibovespa.png', 'rb') as content_file:
+    content = content_file.read()
+    msg.add_attachment(content, maintype='application', subtype='png', filename='ibovespa.png')
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    
+    smtp.login(email, senha)
+    smtp.send_message(msg)
+
